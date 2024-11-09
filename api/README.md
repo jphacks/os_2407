bce2b231502e40cc80673c8c04aa27363885be51
Hietan <hietan622@gmail.com>
2024-11-09 14:18:49 +0900

    Replaced copied directory management with Git submodules for direct linking

--- api/README.md
+++
@@ -1,9 +0,0 @@
-# eventpix-api
-
-API server for Eventpix
-
-## 起動方法
-
-```bash
-$ docker compose up --build
-```
--- mobile/android/app/src/main/kotlin/com/example/eventpix_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.eventpix_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
